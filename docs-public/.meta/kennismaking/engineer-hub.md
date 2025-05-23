b93a7c6cfbcf82e65a4ecd7b412b1e5ba10af9eb
Serge Kroes <serge@kroescontrol.nl>
2025-05-23 16:19:05 +0200

    Revert "kleine cleanup"
    This reverts commit 5ad41464e809f29e382f7348cfb9445d68335fc3.

--- docs-public/.meta/kennismaking/engineer-hub.md
+++ docs-public/.meta/kennismaking/engineer-hub.md
@@ -7,9 +7,9 @@ Schrijf een introductiepagina over de Engineer Hub. Gebruik informatie uit 45 En
 
 Voor het maken van deze documentatie:
 - Gebruik ALLE beschikbare documentatie in de `/old/` directory als bron
-- Bekijk ook relevante bestaande documentatie in `/docs-public/kennismaking/` voor context
+- Bekijk ook relevante bestaande documentatie in `/docs/public/kennismaking/` voor context
 - Let vooral op bestanden in dezelfde directory voor een consistente stijl en aanpak
 ```
 
 ## Aantekeningen
-Dit document is onderdeel van de nieuwe documentatiestructuur. De inhoud moet worden opgesteld op basis van de bestaande documentatie uit /old/ en relevante bestanden in /docs-public/, maar geherstructureerd volgens de nieuwe opzet.
+Dit document is onderdeel van de nieuwe documentatiestructuur. De inhoud moet worden opgesteld op basis van de bestaande documentatie uit /old/ en relevante bestanden in /docs/public/, maar geherstructureerd volgens de nieuwe opzet.
---
+++ process_templates.sh
@@ -0,0 +1,88 @@
+#\!/bin/bash
+
+# Process budgetten files
+echo "Processing budgetten files..."
+
+# Algemeen
+echo '---
+title: Calculaties
+sidebar_position: 1
+description: Informatie over budgetcalculaties en -berekeningen bij Kroescontrol
+tags: [internal, budgetten, calculaties]
+keywords: [calculaties, berekeningen, budgetten, formules]
+last_update:
+  date: 2025-05-22
+  author: System
+image: /img/logo.svg
+---
+
+# Calculaties
+
+Deze pagina bevat informatie over budgetcalculaties en -berekeningen bij Kroescontrol.
+
+## Inhoud wordt nog aangevuld
+
+De documentatie voor deze sectie wordt binnenkort toegevoegd.' > docs/internal/budgetten/algemeen/calculaties.md
+
+echo '---
+title: Wisselen
+sidebar_position: 2
+description: Informatie over het wisselen tussen budgetmodellen bij Kroescontrol
+tags: [internal, budgetten, wisselen]
+keywords: [wisselen, budgetmodellen, overstappen, procedures]
+last_update:
+  date: 2025-05-22
+  author: System
+image: /img/logo.svg
+---
+
+# Wisselen
+
+Deze pagina bevat informatie over het wisselen tussen budgetmodellen bij Kroescontrol.
+
+## Inhoud wordt nog aangevuld
+
+De documentatie voor deze sectie wordt binnenkort toegevoegd.' > docs/internal/budgetten/algemeen/wisselen.md
+
+# FreelanceControl
+echo '---
+title: FreelanceControl
+sidebar_position: 1
+description: Overzicht van het FreelanceControl budgetmodel bij Kroescontrol
+tags: [internal, budgetten, freelancecontrol]
+keywords: [freelancecontrol, budgetmodel, freelancers]
+last_update:
+  date: 2025-05-22
+  author: System
+image: /img/logo.svg
+---
+
+# FreelanceControl
+
+Deze sectie bevat informatie over het FreelanceControl budgetmodel bij Kroescontrol.
+
+## Inhoud wordt nog aangevuld
+
+De documentatie voor deze sectie wordt binnenkort toegevoegd.' > docs/internal/budgetten/freelancecontrol/index.md
+
+echo '---
+title: Facturatie
+sidebar_position: 2
+description: Informatie over facturatie binnen het FreelanceControl model
+tags: [internal, budgetten, freelancecontrol, facturatie]
+keywords: [facturatie, freelancecontrol, facturen, procedures]
+last_update:
+  date: 2025-05-22
+  author: System
+image: /img/logo.svg
+---
+
+# Facturatie
+
+Deze pagina bevat informatie over facturatie binnen het FreelanceControl model.
+
+## Inhoud wordt nog aangevuld
+
+De documentatie voor deze sectie wordt binnenkort toegevoegd.' > docs/internal/budgetten/freelancecontrol/facturatie.md
+
+echo "Templates created successfully\!"
